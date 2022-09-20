16310fcf5c1d19f4c5b5ed0f3dc413c23d8650bb
ericcitaire <eric.citaire@gmail.com>
2022-09-20 16:22:01 +0000

    cgroupns =host

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run --rm -it --name jenkins -p 8080:8080 -p 50000:50000 -u $(id -u):$(id -g) -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd)/jenkins.yaml:/var/jenkins_home/jenkins.yaml ready2use-jenkins
+# https://issues.jenkins.io/browse/JENKINS-67659?jql=resolution%20is%20EMPTY%20and%20component%3D20625
+# The solution is to use --cgroupns host into the container creation command.
+
+docker run --rm -it --name jenkins --cgroupns host -p 8080:8080 -p 50000:50000 --group-add $(id -g) -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd)/jenkins.yaml:/var/jenkins_home/jenkins.yaml ready2use-jenkins
