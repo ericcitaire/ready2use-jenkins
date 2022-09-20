0a68baeea6a063876bee1ef67a1efbadd5242a5c
ericcitaire <eric.citaire@gmail.com>
2022-09-20 12:23:38 +0000

    JCasC

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it --name jenkins -p 8080:8080 ready2use-jenkins
+docker run --rm -it --name jenkins -p 8080:8080 -v $(pwd)/jenkins.yaml:/var/jenkins_home/jenkins.yaml ready2use-jenkins
