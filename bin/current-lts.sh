f09f348654abbb643e1225deab7ebcc756564189
ericcitaire <eric.citaire@gmail.com>
2022-09-22 14:36:52 +0000

    Adds plugins

--- bin/current-lts.sh
+++ bin/current-lts.sh
@@ -13,7 +13,7 @@ suggested_plugins_url="https://raw.githubusercontent.com/jenkinsci/jenkins/jenki
 
 suggested_plugins=$(curl -fsSL "${suggested_plugins_url}" | jq --raw-output '.[].plugins[] | select(.suggested) | .name' | xargs)
 
-docker exec -it jenkins-plugins /bin/sh -c "jenkins-plugin-cli --plugins ${suggested_plugins} gitlab-branch-source docker-workflow blueocean configuration-as-code configuration-as-code-groovy"
+docker exec -it jenkins-plugins /bin/sh -c "jenkins-plugin-cli --plugins ${suggested_plugins} gitlab-branch-source docker-workflow blueocean job-dsl configuration-as-code configuration-as-code-groovy build-monitor-plugin http_request pipeline-utility-steps ssh-agent ansicolor"
 
 docker exec -it jenkins-plugins /bin/sh -c "jenkins-plugin-cli --list --output YAML > /tmp/plugins.yml"
 
